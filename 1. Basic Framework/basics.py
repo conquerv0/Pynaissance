a5205b0e004efd4c00807085290e35ef5282b940
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-04-18 23:39:24 +0800

    Update basics.py

--- 1. Basic Framework/basics.py
+++ 1. Basic Framework/basics.py
@@ -20,16 +20,26 @@ print(df.head())
 df.to_csv('tsla')
 df = pd.read_csv('tsla.csv', parse_dates = True, index_col = 0)
 
-# Basic plotting
-df.plot()
-plt.show()
-
 # Or we can choose to plot only one variable.
 df['Adj Close'].plot()
 
 # Creating data columns through calculation. 
-df['100ma'] = df['Adj Close'].rolling(window=100).mean()
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
 df.dropna(inplace=True)
 # inplace = true allows us to manipulate the df variable directly. 
+print(df.head())
 
 
+# II. BASIC PLOTTING.
+
+df.plot()
+plt.show()
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan = 5, colspan = 1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 1, colspan = 1, sharex=1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
+plt.show()
