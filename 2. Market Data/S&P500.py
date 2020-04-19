6e26e662163aca0e38d2544c2961f069eee9605d
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-04-20 01:38:38 +0800

    Update S&P500.py

--- 2. Market Data/S&P500.py
+++ 2. Market Data/S&P500.py
@@ -1,1 +1,22 @@
+# Basic Imports
+import bs4 as bs
+import pickle
+import request
 
+# Some information about beautiful soup: To be updated. 
+def sp500_tickers():
+  resp = request.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+  soup = bs.BeautifulSoup(resp.text)
+  table = soup.find('table', {'class': 'wikitable sortable'})
+  tickers = []
+  
+  for row in table.findAll('tr')[1:]:
+    ticker = row.findAll('td')[0].text
+    tickers.append(ticker)
+    
+  with open('sp500tickers.pickle', 'wb') as f:
+    pickle.dump(tickers, f)
+     
+  print(tickers)
+    
+  return tickers
