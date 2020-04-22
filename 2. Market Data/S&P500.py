77e02960558d7d3fbdfc90afe431d5c230c124d5
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-04-22 23:03:34 +0800

    Update S&P500.py

--- 2. Market Data/S&P500.py
+++ 2. Market Data/S&P500.py
@@ -47,4 +47,28 @@ def get_yahoo_data(reload_sp500=False):
       print('Always have {}'.format(ticker))
       
  get_yahoo_data()
-      
+ 
+def compile_data():
+  with open('sp500tickers.pickle', 'rb') as f:
+    tickers = pcikle.load(f)
+   
+  main_df = pd.DataFrame()
+  
+  for count, ticker in enumerate(tickers):
+    df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+    df.set_index('Date', inplace = True)
+    
+    df.rename(columns = {'Adj Close', ticker}, inplace=True)
+    df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+    
+    if main_df.empty:
+      main_df = df
+    else:
+      main_df = main_df.join(df, how='outer')
+    
+    if count % 10 == 0:
+      print(count)
+    
+  print(main_df.head())
+  main_df.to_csv('sp500_closes.csv')
+  
