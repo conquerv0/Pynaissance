9ea83f9d37a5db6f2bf0a17733053e72192273b7
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-04-24 07:08:06 +0800

    Update S&P500.py

--- 2. Market Data/S&P500.py
+++ 2. Market Data/S&P500.py
@@ -1,11 +1,19 @@
 # Basic Imports
 import bs4 as bs
+import matplot.pyplot as plt
+from matplotlib import style
+import numpy as np
 import pickle
 import request
 import datetime as dt
 import os
 import pandas as pd
 import pandas_datareader.data as web
+
+style.use('ggplot')
+
+# I. Market Data
+# Scrapping market data from yahoo and compile it into one dataframe.
 
 # Some information about beautiful soup: To be updated. 
 def sp500_tickers():
@@ -73,4 +81,32 @@ def compile_data():
     
   print(main_df.head())
   main_df.to_csv('sp500_closes.csv')
+ 
+#II. Data Visualization with correlation heat map.
+
+def visualize_data():
+  """"""
+  df = pd.read_csv('sp500_joined_closes.csv')
+  df_corr = df.corr()
+  
+  data = df_corr.values
+  fig = plt.figure()
+  ax = fig.add_subplot(1, 1, 1) # An one by one plot.
+  
+  heatmap = ax.pcolor(data, cmap = plt.cm.RdYlGn)
+  fig.colorbar(heatmap)
+  ax.set_xticks(np.arange(data.shape[0])+ 0.5, minor=False)
+  ax.set_yticks(np.arange(data.shape[1])+0.5, minor=False)
+  ax.inverst_yaxis()
+  ax.xaxis.tick_top()
+  
+  column_labels = df_corr.columns
+  row_labels = df_corr.index
+  
+  ax.set_xticklables(column_labels)
+  ax.set_yticklabels(row_labels)
+  plt.xticks(rotation=90)
+  heatmap.set_clim(-1, 1)
+  
+visualize_data()
   
