fd0c85b39bacea7078f163d4ec32f21c4bc06526
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-04-27 23:21:09 +0800

    Update dataprep.py

--- 3. Machine Learning/dataprep.py
+++ 3. Machine Learning/dataprep.py
@@ -1,1 +1,14 @@
+# Basic Import Library
+import numpy as np
+import pandas as pd
+import pickle
 
+def process_data_for_labels(ticker):
+  days = 7 # the training time period at which we are interested in rebalancing
+  df = pd.read_csv('sp500.closes.csv' index_col=0)
+  tickers = df.columns.values.tolist()
+  df.fillna(0, inplace=True)
+  
+  for i in range(1, days+1):
+    # we need to shift the future data to the upper (older) column
+    df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker] )
