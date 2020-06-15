8885e95f92629eae6fbb3b0129de01adeab16a82
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-06-14 22:54:38 -0400

    Algo Update: Techinicals-BollingerBand

--- 3. Technicals/bollingerband.py
+++ 3. Technicals/bollingerband.py
@@ -13,3 +13,14 @@ capital_base  = 100000
 t1 = 50 # Length of Moving Average window.
 t2 = 30 # Length of Rate of Change(ROC) window.
 MaxBar = 0.8 # When reach max holding period, selling point. 
+
+def initialize(account):
+    pass
+
+def handle_data(account):    
+    #print account.current_date
+    cal = Calendar('China.SSE')
+    last_dayt1 = cal.advanceDate(account.current_date, str(-t1)+'B', BizDayConvention.Preceding).toDateTime()           #计算出前t1个交易日
+    buylist = []
+    selllist = []
+
