7464d768b084f4c2171863d3c32de69cd2e3f4c8
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-06-14 22:57:11 -0400

    AlgoUpdate: Technicals-BollingerBand

--- 3. Technicals/bollingerband.py
+++ 3. Technicals/bollingerband.py
@@ -8,19 +8,23 @@ import math
 start = datetime(2015, 1, 1)
 end = datetime(2019, 5, 31)
 benchmark = 'SPY'
+universe = set_universe('SP500')
 capital_base  = 100000
 
 t1 = 50 # Length of Moving Average window.
 t2 = 30 # Length of Rate of Change(ROC) window.
 MaxBar = 0.8 # When reach max holding period, selling point. 
 
+T =  pd.Series(data=[t1],index = universe)
+commission = Commission(buycost=0.0003, sellcost=0.0003) 
+
 def initialize(account):
     pass
 
 def handle_data(account):    
-    #print account.current_date
-    cal = Calendar('China.SSE')
-    last_dayt1 = cal.advanceDate(account.current_date, str(-t1)+'B', BizDayConvention.Preceding).toDateTime()           #计算出前t1个交易日
+    cal = Calendar('NYSE') # Account for API difference
+    last_dayt1 = cal.advanceDate(account.current_date, str(-t1)+'B', BizDayConvention.Preceding).toDateTime()           
+    # Calculate t1 trading day. 
     buylist = []
     selllist = []
 
