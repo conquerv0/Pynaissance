56a0c8858471eded98d2f2cff404fd506ea462b0
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-06-15 20:05:13 -0400

    AlgoUpdate: Fundamentals-FactorAnalysis

--- 4. Fundamentals/factor_analysis.py
+++ 4. Fundamentals/factor_analysis.py
@@ -1,1 +1,11 @@
+# Basic Setup with Quantopian alphalens.
 
+import numpy as np
+import pandas as pd
+from quantopian.research import run_pipeline
+from quantopian.pipeline import Pipeline
+from quantopian.pipeline.data.builtin import USEquityPricing
+from quantopian.pipeline.factors import CustomFactor, Returns
+from quantopian.pipeline.classifiers.fundamentals import Sector  
+from quantopian.pipeline.filters import QTradableStocksUS
+from time import time
