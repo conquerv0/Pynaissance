8bd560dd21d7d9f0ac4213e8891c50fd6fe4e767
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-07-04 23:45:01 +0800

    AlgoUpdate: CAPM-FrontierOptimization

--- 4. Fundamentals/capm.py
+++ 4. Fundamentals/capm.py
@@ -37,3 +37,33 @@ def rand_portfolio(returns):
   if mean > 2:
     return rand_portfolio(returns)
   return mean, sd
+
+def optimal_portfolio(returns):
+  n = len(returns)
+  returns = np.asmatrix(returns)
+  
+  N = 100000
+  
+  mean_s = [100**(5.0*t/N - 1.0) for t in range(N)]
+  # Convert to cvxopt matrices
+  S = opt.matrix(np.cov(returns))
+  p_bar = opt.matrix(np.mean(returns, axis=1))
+  
+  # Create constraint matrices
+  G = -opt.matrix(np.cov(returns))
+  pbar = opt.matrix(np.mean(returns, axis=1))
+  
+  # Create constraint matrices
+  G = -opt.matrix(np.eye(n))
+  h = opt.matrix(0.0, (n, 1))
+  A = opt.matrix(1.0, (1. n))
+  b = opt.matrix(1.0)
+  
+  # Calculate efficient frontier weights using quadratic programming
+  portfolios = [solvers.qp(mean*S, -p_bar, G, h, A, b)['x'] for mean in mean_s]
+  
+  # Calculate the risk and returns of the frontier.
+  returns = [blas.dot(pbar, x) for x in portfolios]
+  risks = [np.sqrt(blas.dot(x, S*x)) for x in portfolios]
+  
+  return returns, risks
