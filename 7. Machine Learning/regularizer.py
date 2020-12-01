b0902c205216321a0be626d3cd415d3f23ecb51e
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-12-01 16:34:32 +0800

    AlgoUpdate: ML-Regularizer

---
+++ 7. Machine Learning/regularizer.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+class Regularizer:
+    def __init__(self, param):
+        self._param = param
+
+    def regularize(self, W):
+        return 0
+
+class L1Regularizer(Regularizer):
+    def __init__(self, param):
+        '''
+        Parameters
+        ----------
+        param : Regularization Penalty
+        '''
+        super().__init__(param)
+
+    def regularize(self, W):
+        '''
+        Parameters
+        ----------
+        W : Weights of model
+        Returns
+        -------
+        W : Weights regularized
+        '''
+        return self._param * np.where(W >= 0, 1, -1)
+
+class L2Regularizer(Regularizer):
+    def __init__(self, param):
+        '''
+        Parameters
+        ----------
+        param : Regularization Penalty
+        '''
+        super().__init__(param)
+
+    def regularize(self, W):
+        '''
+        Parameters
+        ----------
+        W : Weights of model
+        Returns
+        -------
+        W : Weights regularized
+        '''
+        return self._param * W
+
+class ElasticRegularizer(Regularizer):
+    def __init__(self, param, ratio):
+        '''
+        Parameters
+        ----------
+        param : Regularization Penalty
+        ratio : The ElasticNet mixing parameter
+        '''
+        self.__l1 = L1Regularizer(param)
+        self.__l2 = L2Regularizer(param)
+        self.__ratio = ratio
+
+    def regularize(self, W):
+        '''
+        Parameters
+        ----------
+        W : Weights of model
+        Returns
+        -------
+        W : Weights regularized
+        '''
+        return self.__ratio * self.__l1.regularize(W) + (1 - self.__ratio) * self.__l2.regularize(W)
