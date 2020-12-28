1c6d6f3fcbed2783d2ae3f5fad051db8dc564e8f
Victor Xiao <vjf.xiao@mail.utoronto.ca>
2020-12-28 20:17:21 +0800

    ToolsUpdate: ML-DS, Minor Issue Addressed.

--- 7. Machine Learning/tools/decision_stump.py
+++ 7. Machine Learning/tools/decision_stump.py
@@ -2,26 +2,24 @@ import numpy as np
 
 class DecisionStump():
     def update_parameter(self, h, feature_index, threshold, direction, err_val):
-        self.min_err_val = err_val
-        self.feature_index = feature_index
-        self.threshold = threshold
-        self.direction = direction
+      self.min_err_val = err_val
+      self.feature_index = feature_index
+      self.threshold = threshold
+      self.direction = direction
       
   
     def select_direction(self, feature_index, threshold, X, y, w):
-        """
-        """
-        for direction in ["greater", "less"]:
+        for direction in ['greater', 'less']:
             h = np.ones_like(y)
-          
-        if direction == "greater":
-            h[np.flatnonzero(X[:, feature_index] < threshold)] = -1
-        else:
-            h[np.flatnonzero(X[:, feature_index] >= threshold)] = -1    
-        
-        err_val = np.sum(w[np.flatnonzero(y != h)])
-        if err_val < self.min_err_val:
-            self.update_parameter(h, feature_index, threshold, "greater", err_val)
+
+            if direction == 'greater':
+                h[np.flatnonzero(X[:, feature_index] < threshold)] = -1
+            else:
+                h[np.flatnonzero(X[:, feature_index] >= threshold)] = -1
+
+            err_value = np.sum(w[np.flatnonzero(h != y)])
+            if err_value < self.min_err_val:
+                self.update_parameter(h, feature_index, threshold, 'greater', err_value)
           
     def select_threshold(self, feature_index, X, y, w):
         n_samples = X.shape[0]
