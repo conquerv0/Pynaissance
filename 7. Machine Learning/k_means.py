0d69f7e0a454e7608e4c70f87276e4b57417cf1a
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-12-28 11:17:10 +0800

    AlgoUpdate: ML-KMeans

---
+++ 7. Machine Learning/k_means.py
@@ -0,0 +1,44 @@
+import numpy as np
+import distance
+
+
+class KMeans:
+    @property
+    def centers(self):
+        return self.__centers
+
+    def fit(self, X, n_clusters, epochs):
+        """
+        Parameters
+        ----------
+        X : shape (n_samples, n_features)
+            Training data
+        n_clusters : The number of clusters
+        epochs : The number of epochs
+        Returns
+        -------
+        y : shape (n_samples,)
+            Predicted cluster label per sample.
+        """
+        n_samples = X.shape[0]
+        self.__centers = X[np.random.choice(n_samples, n_clusters)]
+
+        for _ in range(epochs):
+            labels = self.predict(X)
+            self.__centers = [np.mean(X[np.flatnonzero(labels == i)], axis=0) for i in range(n_clusters)]
+
+        return self.predict(X)
+
+    def predict(self, X):
+        """
+        Parameters
+        ----------
+        X : shape (n_samples, n_features)
+            Predicting data
+        Returns
+        -------
+        y : shape (n_samples,)
+            Predicted cluster label per sample.
+        """
+        distances = np.apply_along_axis(distance.euclidean_distance, 1, self.__centers, X).T
+        return np.argmin(distances, axis=1)
