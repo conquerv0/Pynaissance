b5ff4f14a518fbd58ba19d97e861d51f4a1e7327
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-12-28 20:06:06 +0800

    AlgoUpdate: ML-Tools, Decision Stump

---
+++ 7. Machine Learning/tools/decision_stump.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class DecisionStump():
+    def update_parameter(self, h, fearture_index, threshold, direction, err_val):
+      self.min_err_val = err_val
+      self.feature_index = feature_index
+      self.threshold = threshold
+      self.direction = direction
+      
+  
+    def select_direction(self, feature_index, threshold, X, y, w):
+      """
+      """
+      for direction in ["greater", "less"]:
+         h = np.ones_like(y)
+          
+         if direction == "greater":
+             h[np.flatnonzero(X[:, feature_index] < threshold)] = -1
+         else:
+             h[np.flatnonzero(X[:, feature_index] >= threshold)] = -1
+          
+         err_val < self.min_err_val:
+             self.update_parameter(h, feature_index, threshold, "greater", err_val)
+          
+    def select_threshold(self, feature_index, X, y, w):
+       n_samples = X.shape[0]
+       
+       for i in range(n_samples):
+         self.select_direction(feature_index, X[i, feature_index], X, y, w)
