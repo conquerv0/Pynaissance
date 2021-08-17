2a67a6820bf98764f2a4cf4678957b9e4e136db3
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2021-08-17 10:18:02 +0800

    AlgoUpdate: FactorOptimization with nn
    setting up template for implementation

---
+++ 7. Machine Learning/factor_nn.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.data import TensorDataset
+
+def data_parser(filepath):
+  pass
+
+def data_preproc(dataset):
+  pass
+
+def net(nn.Module):
+  def __init__(self):
+    super(net, self).__init__()
+    
+def train:
+  pass
+
+if __name__=="__main__":
+  pass
