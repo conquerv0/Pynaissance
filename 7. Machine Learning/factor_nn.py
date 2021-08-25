cb7c9d3c8350c5b68dfbe97ec8aef0c01dc46fa4
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2021-08-25 14:06:05 +0800

    Update factor_nn.py with Training Module

--- 7. Machine Learning/factor_nn.py
+++ 7. Machine Learning/factor_nn.py
@@ -6,6 +6,8 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
 
+""" == Data Preproc Module =="""
+
 def data_parser(filepath):
     dataset = {}
     
@@ -26,12 +28,58 @@ def data_parser(filepath):
 def data_preproc(dataset):
   pass
 
-def net(nn.Module):
-  def __init__(self):
-    super(net, self).__init__()
+""" == Training Module == """
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+         self.layers = nn.Sequential(
+            nn.Linear(416, 100),
+            nn.Tanh(),
+            nn.Linear(100, 10),
+            nn.Tanh(),
+            nn.Linear(10, 1)
+        )
+    def forward(self, x):
+        x = self.layers(x)
+        return x
     
-def train:
-  pass
+def train(model, device, dataloader, epochs=200)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+    loss_func = nn.MSELoss()
+    mse, corrs = [], []
+    
+    print('Start training model...')
+    
+    for epoch in range(epochs):
+        for x, y in dataloader:
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = loss_func(y_hat, y)
+            
+            if device == torch.device('cuda'):
+                y_hat = y_hat.cpu().data.numpy()
+                y = y.cpu().data.numpy()
+            corr = np.corrcoef(y_hat.flatten(), y.flatten())
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        mse.append(loss)
+        corrs.append(corr)
+        
+        if epoch % 10 == 0:
+            print('Step: {}, Loss: {}, Correlation: {}'.format(epoch, loss.item(), corrs[-1]))
+        if epoch % 20 == 0:
+            plt.plot(range(len(corrs)), corrs, label='correlation')
+            plt.show()
+        
+    fig, axs = plt.subplots(2, sharex=True, tightlayout=True)
+    axs[0].plot(range(epochs), corrs, label='correlation')
+    axs[1].plot(range(epochs), mse, color='salmon', label='mse')
+    plt.title('Change in Correlation and MSE')
+    
+    return model
 
 if __name__=="__main__":
   pass
