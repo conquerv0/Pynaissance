9c093a7d4a39a29a1df3d9112beb629d3dd62c6a
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2021-09-22 17:59:51 +0800

    ModelUpdate: ResNet Architecture

--- 7. Machine Learning/resnet_regression.py
+++ 7. Machine Learning/resnet_regression.py
@@ -2,7 +2,6 @@ import pytorch as torch
 import numpy as np
 import matplotlib as plt
 import torch.nn as nn
-from sklearn.linear_model import Ridge
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
 from sklearn.metrics import mean_squared_error
@@ -16,12 +15,10 @@ class IdentityBlock(nn.Module):
       self.act = nn.Tanh()
       self.width = WIDTH
       self.layer1 = nn.Linear(WIDTH, WIDTH)
-      self.layer2 = nn.Linear(WIDTH, WIDTH)
       self.batch_norm = nn.BatchNorm1d(WIDTH)
       
   def forward(self, x):
       x = self.act(self.batch_norm(self.layer1(x)))
-      x = self.act(self.batch_norm(self.layer2(x) + x))
       return x
     
 
@@ -35,11 +32,32 @@ class Block(nn.Module):
       self.batch_norm = nn.BatchNorm1d(WIDTH)
       
  def forward(self, x):
-      x = self.act(self.batch_norm(self.layer1(x)))
-      x = self.act(self.batch_norm(self.layer2(x)))
-      shortcut = self.act(self.batch_norm(self.layer1(x)))
+      x = self.act(self.layer1(x))
+      x = self.act(self.layer2(x))
+      shortcut = self.act(self.layer1(x))
       x = self.act(x + shortcut)
       return x
     
- 
+class ResNet(nn.Module);
+  def __init__(self, input_dim, width=WIDTH, layer=2):
+      super().__init__()
+      self.width = width
+      self.input_dim = input_dim
+      self.layers = nn.ModuleList()
+      self.depth = layer
+      
+      self.layers.append(nn.Linear(input_dim, width))
+      for _ in range(self.depth):
+        self.layers.append(IdentityBlock(width))
+        self.layers.append(IdentityBlock(width))
+        self.layers.append(Block(width, width))
+        
+      self.fc = nn.Linear(Block(width, width))
+  
+  def forward(self, x):
+      for layer in self.layers:
+        x = layer(x)
+      x = self.fc(x)
+      return x
+      
    
