4bab6d38a9deec1441105805ea6cb8402c4b6f05
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2021-09-22 14:27:36 +0800

    ModelUpdate_ResNet_Regression

--- 7. Machine Learning/resnet_regression.py
+++ 7. Machine Learning/resnet_regression.py
@@ -12,4 +12,34 @@ WIDTH = 50
 
 class IdentityBlock(nn.Module):
   def __init__(self, width = WIDTH):
+      super().__init__()
+      self.act = nn.Tanh()
+      self.width = WIDTH
+      self.layer1 = nn.Linear(WIDTH, WIDTH)
+      self.layer2 = nn.Linear(WIDTH, WIDTH)
+      self.batch_norm = nn.BatchNorm1d(WIDTH)
+      
+  def forward(self, x):
+      x = self.act(self.batch_norm(self.layer1(x)))
+      x = self.act(self.batch_norm(self.layer2(x) + x))
+      return x
+    
+
+class Block(nn.Module):
+  def __init__(self, input_dim, width=WIDTH):
+      super().__init__()
+      self.act = nn.Tanh()
+      self.input_dim = input_dim
+      self.layer1 = nn.Linear(input_dim, WIDTH)
+      self.layer2 = nn.Linear(WIDTH, WIDTH)
+      self.batch_norm = nn.BatchNorm1d(WIDTH)
+      
+ def forward(self, x):
+      x = self.act(self.batch_norm(self.layer1(x)))
+      x = self.act(self.batch_norm(self.layer2(x)))
+      shortcut = self.act(self.batch_norm(self.layer1(x)))
+      x = self.act(x + shortcut)
+      return x
+    
+ 
    
